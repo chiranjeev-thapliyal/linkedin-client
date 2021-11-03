257ad239eedb831211690a5b6b8f408e1594a571
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-03 19:15:22 +0530

    Create ProfileAbout.jsx

---
+++ src/components/ali/userProfile/ProfileAbout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function ProfileAbout() {
+    return (
+        <div className="profileAboutMainDiv">
+            <h1>Testing profileAbout.jsx</h1>
+
+            <div className="flex-sb">
+
+                <h2>About</h2>
+                <img src="icons/editButton.svg" alt="editButton" />
+
+            </div>
+
+
+
+
+
+
+
+        </div> // profileAboutMainDiv 
+    )
+}
