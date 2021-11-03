77b8f025f404814e11f3a2e6783fc9cee184658b
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-03 19:15:06 +0530

    Update Profile.jsx

--- src/components/ali/userProfile/Profile.jsx
+++ src/components/ali/userProfile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProfileAbout from './ProfileAbout'
 import ProfileFrontCard from './ProfileFrontCard'
 
 export default function Profile() {
@@ -21,6 +22,7 @@ export default function Profile() {
             profilePeople you may know 
             */}
             <ProfileFrontCard />
+            <ProfileAbout />
 
 
         </div>// End of profileMainDiv 
