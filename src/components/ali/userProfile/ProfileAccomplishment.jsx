d16dfb60f7b2cad545bb83b64c0c2213c2fec9a6
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-04 22:54:57 +0530

    Create ProfileAccomplishment.jsx

---
+++ src/components/ali/userProfile/ProfileAccomplishment.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import CardHeader from './CardHeader'
+import Divider from './Divider'
+import ProfileAccomplishmentCard from './ProfileAccomplishmentCard'
+
+export default function ProfileAccomplishment() {
+
+    const data = [
+        {
+            title: "Languages",
+            subTitle: ["English", "Hindi", "Urdu"],
+
+        },
+        {
+            title: "Honor & Award",
+            subTitle: ["Most Popular Web App Award ( Construct week )"],
+
+
+        }
+    ]
+
+
+
+
+    return (
+        <div className="ProfileAccomplishmentMainDiv PMD">
+
+
+            <CardHeader title={"Accomplishments"} image1={"icons/plusButton.svg"} />
+
+            {
+                data.map((e, i) => (
+
+                    <div>
+
+                        <ProfileAccomplishmentCard title={e.title} subTitle={e.subTitle} />
+
+                        <div className="ml2rem">
+
+                            <br />
+                            {i < data.length - 1 ? <Divider /> : ""}
+                        </div>
+                    </div>
+                ))
+            }
+
+
+
+
+        </div>
+    )
+}
