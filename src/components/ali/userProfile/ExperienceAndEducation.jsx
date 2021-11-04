cf24331365ff1b5b595edf9fc45c8694e58709e6
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-04 12:42:44 +0530

    Create ExperienceAndEducation.jsx

---
+++ src/components/ali/userProfile/ExperienceAndEducation.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function ExperienceAndEducation() {
+    return (
+        <div className="ExperienceAndEducationMainDiv PMD">
+
+            {/* Experience section starts here */}
+            <div className="EAE-Experience">
+
+                <div className="ProfileActivityHeader flex-sb PAMD-Header editButton">
+                    <h2>Experience</h2>
+                    <img src="icons/plusButton.svg" alt="plusButton" />
+                </div>
+
+            </div> {/* EAE-Experience */}
+
+            {/* Education section starts here */}
+            <div className="EAE-Education">
+
+                <div className="ProfileActivityHeader flex-sb PAMD-Header editButton">
+                    <h2>Education</h2>
+                    <img src="icons/plusButton.svg" alt="plusButton" />
+                </div>
+
+
+
+                {/* ExperienceAndEducationCard */}
+
+
+            </div>  {/* EAE-Education */}
+
+
+
+        </div>
+    )
+}
