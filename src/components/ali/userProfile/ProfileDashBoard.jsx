35cd7a7721822898ce7c49dc42e23e45b3aa79fe
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-09 19:06:03 +0530

    Update ProfileDashBoard.jsx

--- src/components/ali/userProfile/ProfileDashBoard.jsx
+++ src/components/ali/userProfile/ProfileDashBoard.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import CardHeader from './CardHeader'
+import ProfileDashboardBottom from './ProfileDashboardBottom'
+import ProfileDashboardMid from './ProfileDashboardMid'
 
 export default function ProfileDashBoard() {
+
+
+
+
     return (
-        <div className="ProfileDashBoard PMD">
-
-            <CardHeader title="Your Dashboard" />
-            <CardHeader title="Private to you" image1={"icons/star.png"} subTitle=" All Star" />
+        <div className="ProfileDashBoard PMD ">
 
 
-        </div>
+            <CardHeader title="Your Dashboard" subTitle="Private to you" image1={"icons/star.svg"} starTitle=" All Star" />
+
+            <ProfileDashboardMid />
+            <ProfileDashboardBottom />
+
+
+        </div >
     )
 }
