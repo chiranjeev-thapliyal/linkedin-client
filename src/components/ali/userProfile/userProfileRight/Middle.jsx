2e92abd99bc81de25e4f084b0f867cacf3c23a2e
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-09 20:01:58 +0530

    Create Middle.jsx

---
+++ src/components/ali/userProfile/userProfileRight/Middle.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CardHeader from '../CardHeader'
+
+export default function Middle() {
+    return (
+        <div className="userProfileRightMiddle UPR">
+
+            <h3 >People also viewed</h3>
+            <strong> <p >People also viewed</p></strong>
+
+            <div>
+            </div>
+
+        </div>
+    )
+}
