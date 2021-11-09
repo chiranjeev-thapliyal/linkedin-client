7164efe1a65866b779d48cb7d82600c4777f2bee
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-09 13:16:24 +0530

    Update Profile.jsx

--- src/components/ali/userProfile/Profile.jsx
+++ src/components/ali/userProfile/Profile.jsx
@@ -3,7 +3,10 @@ import ExperienceAndEducation from './ExperienceAndEducation'
 import ProfileAbout from './ProfileAbout'
 import ProfileAccomplishment from './ProfileAccomplishment'
 import ProfileActivity from './ProfileActivity'
+import ProfileDashBoard from './ProfileDashBoard'
+import ProfileFooter from './ProfileFooter'
 import ProfileFrontCard from './ProfileFrontCard'
+import ProfileInterests from './ProfileInterests'
 import SkillsAndEndorsements from './SkillsAndEndorsements'
 
 export default function Profile() {
@@ -18,9 +21,9 @@ export default function Profile() {
             profileFeatured
            // profileActivity
            // profileExperienceAndEducation
-            profileSkills&Endorsements
-            profileAccomplishments
-            profileInterests
+            // profileSkills&Endorsements
+            // profileAccomplishments
+            // profileInterests
             profileEdit public profile & URL
             profilePeople also viewed
             profilePeople you may know 
@@ -33,14 +36,14 @@ export default function Profile() {
                 <ExperienceAndEducation />
                 <SkillsAndEndorsements />
                 <ProfileAccomplishment />
+                <ProfileInterests />
 
-
-
+                <ProfileDashBoard />
 
                 <ProfileActivity />
             </div>
 
-
+            <ProfileFooter />
         </div>// End of profileMainDiv 
 
     )
