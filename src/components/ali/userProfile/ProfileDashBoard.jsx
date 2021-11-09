7831509f28db0a975807d3419bb8bf8c65f20d9e
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-09 13:16:32 +0530

    Create ProfileDashBoard.jsx

---
+++ src/components/ali/userProfile/ProfileDashBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardHeader from './CardHeader'
+
+export default function ProfileDashBoard() {
+    return (
+        <div className="ProfileDashBoard PMD">
+
+            <CardHeader title="Your Dashboard" />
+            <CardHeader title="Private to you" image1={"icons/star.png"} subTitle=" All Star" />
+
+
+        </div>
+    )
+}
