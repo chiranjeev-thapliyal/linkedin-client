eac749f38f4e2d43d11db96fcf8a0da60b795e71
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-12 13:39:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import './App.css'
-import Route from "./components/Route";
+import { Routes } from './Routes/Routes';
 
-import SignupLogo from "./components/assets/svg/SignupLogo";
-import { NetworksPage } from "./components/views/Network/NetworksPage";
-import { JoinNowPage } from "./components/views/Signup/JoinNowPage";
-import { Signup } from "./components/views/Signup/Signup";
 
 function App() {
   return (
     <div className='App'>
-      {/* <Route /> */}
-      <NetworksPage />
-      {/* <Signup /> */}
-      {/* <JoinNowPage /> */}
+      <Routes />
     </div>
   );
 }
