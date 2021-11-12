e8547ae28ea06ef3b1fa36d0f150148fd5d1453b
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-12 13:40:08 +0530

    Update NetworksPage.jsx

--- src/components/views/Network/NetworksPage.jsx
+++ src/components/views/Network/NetworksPage.jsx
@@ -5,7 +5,7 @@ import { NetworksRight } from "./NetworksRight"
 export const NetworksPage = () => {
 
     return (
-        <div style={{display: "flex", paddingLeft: "7%", paddingTop: "10vh", backgroundColor: "rgb(243,242,239)"}}>
+        <div style={{ display: "flex", paddingLeft: "7%", paddingTop: "10vh", backgroundColor: "rgb(243,242,239)" }}>
             <NetworksLeft />
             <NetworksRight />
         </div>
