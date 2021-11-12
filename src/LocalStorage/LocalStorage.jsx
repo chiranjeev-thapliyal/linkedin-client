8ff48246737ad046930156daa4486d34606c4a74
Rabia-Basari777 <rabia_fw10_200@masai.school>
2021-11-12 20:43:43 +0530

    deleted local storage

--- src/LocalStorage/LocalStorage.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../Contexts/AuthContextProvider";
-
-export const LocalStorage = () => {
-    const { token } = useContext(AuthContext);
-    console.log("Im in localStorage:", token);
-}
-
