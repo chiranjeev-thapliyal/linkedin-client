7aa625758cb8fbeb327bb37c10c6d16acbe06eb5
Himanshu <himanshubisht3112@gmail.com>
2021-11-13 15:12:14 +0530

    pop-up-3

--- src/components/views/Home/ReplyBox.js
+++ src/components/views/Home/ReplyBox.js
@@ -9,7 +9,6 @@ export default function ReplyBox() {
           src="https://media-exp1.licdn.com/dms/image/C4E03AQFmeJIz0DYD9A/profile-displayphoto-shrink_100_100/0/1625409631133?e=1642032000&v=beta&t=wOB4B-QXl_1BwC3YCjmXPx-jiOtkveea1AuTyzbktmc"
           alt=""
         />
-
         <div className="replyContent">
           <p>
             <b>Himanshu Bisht</b>
